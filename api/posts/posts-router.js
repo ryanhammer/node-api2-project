9a101b68194978bd5d839fc583808a54d3d333fb
rhammer35 <rhammer35@icloud.com>
2021-05-04 18:22:42 -0400

    created endpoint for updating a post

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -61,4 +61,36 @@ router.post('/', (req, res) => {
   }
 });
 
+router.put('/:id', (req, res) => {
+  const { id } = req.params;
+  const updatedPost = req.body;
+    if ( !updatedPost.title || !updatedPost.contents ) {
+      res.status(400).json({
+        message: "Please provide title and contents for the post"
+      });
+    }
+  Post.update(id, updatedPost)
+    .then(post => {
+      if (post) {
+        Post.findById(id)
+        .then( newPost => {
+          res.status(201).json(newPost);
+        })
+        .catch(err => {
+          console.log(err);
+        })
+      } else {
+        res.status(404).json({
+          message: "The post with the specified ID does not exist"
+        });
+      }
+    })
+    .catch(error => {
+      console.log(error);
+      res.status(500).json({
+        message: "The post information could not be modified"
+      });
+    });
+});
+
 module.exports = router;
